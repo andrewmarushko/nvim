fe96f066e838de73d8c41d7e08a5157a03400ddf
Andrew Marushko <andrewmarushko@gmail.com>
2022-12-02 11:51:42 +0100

    new setup

---
+++ settings/keymaps.lua
@@ -0,0 +1,67 @@
+vim.g.mapleader = "\\"
+
+local keymap = vim.keymap -- for conciseness
+local status, telescope = pcall(require, "telescope")
+if not status then
+	return
+end
+local function telescope_buffer_dir()
+	return vim.fn.expand("%:p:h")
+end
+-- general keymap
+
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "x", '"_x"')
+
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+-- window management
+keymap.set("n", "sv", "<C-w>v") -- split window vertically
+keymap.set("n", "sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "tp", ":tabp<CR>") --  go to previous tab
+
+-- Plugin Keybinds
+
+-- vim-maximizer
+keymap.set("n", "sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+-- telescope
+keymap.set("n", "ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "fg", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "\\\\", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- telescope git commands (not on youtube nvim video)
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+keymap.set("n", "gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+keymap.set("n", "gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- restart lsp server (not on youtube nvim video)
+keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+keymap.set("n", "sf", function()
+	telescope.extensions.file_browser.file_browser({
+		path = "%:p:h",
+		cwd = telescope_buffer_dir(),
+		respect_gitignore = false,
+		hidden = true,
+		grouped = true,
+		previewer = false,
+		initial_mode = "normal",
+		layout_config = { height = 40 },
+	})
+end)
